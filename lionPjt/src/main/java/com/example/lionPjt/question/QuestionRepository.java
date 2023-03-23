d45e5ffc94805a87527b2e95fd4623fd75d456a9
seol11796 <seol11796@gmail.com>
2023-03-23 14:12:35 +0900

    0323 study

--- lionPjt/src/main/java/com/example/lionPjt/question/QuestionRepository.java
+++ lionPjt/src/main/java/com/example/lionPjt/question/QuestionRepository.java
@@ -1,5 +1,7 @@
 package com.example.lionPjt.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,6 +15,7 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
+    Page<Question> findAll(Pageable pageable);
 
     // @Modifying // 만약 아래 쿼리가 UPDATE, DELETE, INSERT 라면 이걸 붙여야 한다.
     // nativeQuery = true 여야 MySQL 쿼리문법 사용 가능
--- lionPjt/src/main/java/com/example/lionPjt/question/QuestionService.java
+++ lionPjt/src/main/java/com/example/lionPjt/question/QuestionService.java
@@ -2,8 +2,14 @@ package com.example.lionPjt.question;
 
 import com.example.lionPjt.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,8 +19,15 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList(){
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page){
+
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("created"));
+        // page - 조회할 페이지의 번호, 10 - 한 페이지에 보여줄 게시물의 갯수
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
+
+
     }
 
     public Question getQuestion(Integer id){
@@ -24,6 +37,17 @@ public class QuestionService {
         }else{
           throw new DataNotFoundException("question not found");
         }
+    }
+
+
+
+
+    public void create(String subject, String content){
+        Question  q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreated(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 
 }
