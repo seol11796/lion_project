ed6ac2a8a6a4e66ff88e96e6c70b6c652aa31347
seol11796 <seol11796@gmail.com>
2023-03-27 14:42:33 +0900

    0327 study(signup,login,logout)

--- lionPjt/src/main/java/com/example/lionPjt/SecurityConfig.java
+++ lionPjt/src/main/java/com/example/lionPjt/SecurityConfig.java
@@ -2,20 +2,57 @@ package com.example.lionPjt;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests().requestMatchers(
-                new AntPathRequestMatcher("/**")).permitAll();
+                        new AntPathRequestMatcher("/**")).permitAll()
+                .and()
+                    .csrf().ignoringRequestMatchers(
+                        new AntPathRequestMatcher("/h2-console/**"))
+                .and()
+                    .headers()
+                    .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and()
+                    .formLogin()
+                    .loginPage("/user/login")
+                    .defaultSuccessUrl("/")
+                .and()
+                    .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true)
+        ;
 
         return http.build();
     }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // 스프링 시큐리티 인증을 담당. 빈 생성 시 UserSecurityService와 PasswordEncoder가 자동 설정됨.
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
+    {
+        {
+            return authenticationConfiguration.getAuthenticationManager();
+        }
+    }
+
 }
---
+++ lionPjt/src/main/java/com/example/lionPjt/SiteUser/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.lionPjt.SiteUser;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<SiteUser,Long> {
+
+    Optional<SiteUser> findByusername(String username);
+
+
+}
